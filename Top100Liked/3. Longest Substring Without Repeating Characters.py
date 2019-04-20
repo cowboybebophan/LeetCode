cde586b03876f16f10c8bd60f2e0012277b5577e
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-04-20 15:41:20 -0400

    Update 3. Longest Substring Without Repeating Characters.py

--- Top100Liked/3. Longest Substring Without Repeating Characters.py
+++ Top100Liked/3. Longest Substring Without Repeating Characters.py
@@ -10,3 +10,7 @@ class Solution:
             usedChar[s[i]] = i # 无论之前是否出现过，将当前i位置的字符在字典内进行更新
         return maxLength
     
+Note: 如何理解 start <= usedChar[s[i]] ：
+    例如 “tmmzuxta”，在i = 2时因为之前出现过‘m’所以此时将start更新为2，当i = 6时，此时‘t’虽然已经在之前出现过，
+    但是此时start已经在之前一个‘t’的右侧，所以不用进入if循环更新start，而是进入else更新maxLength。
+    如果去掉这个判断，结果为len(‘mzuxt’) = 5；加上判断 结果为len(‘mzuxta’) = 6.
