4d1340bc64c9a86433f7ba0ee279f912aa47d6d9
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-04-28 01:04:04 -0400

    Update 23. Merge k Sorted Lists.py

--- Top100Liked/23. Merge k Sorted Lists.py
+++ Top100Liked/23. Merge k Sorted Lists.py
@@ -12,3 +12,26 @@ Input:
 Output: 1->1->2->3->4->4->5->6
 """
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Using sort()
+class Solution(object):    
+    def mergeKLists(self, lists):
+        l = []
+        for lst in lists:
+            while lst:
+                l.append(lst.val)
+                lst = lst.next
+        l.sort()
+        dummy = head = ListNode(None)
+        for num in l:
+            head.next = ListNode(num)
+            head = head.next
+        return dummy.next
+      
+# Using PriorityQueue      
+      
