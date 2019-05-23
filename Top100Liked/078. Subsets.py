74728552bf19f254daf4966eaaa7abba3e364753
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-05-23 15:08:49 -0400

    Create 078. Subsets.py

---
+++ Top100Liked/078. Subsets.py
@@ -0,0 +1,20 @@
+"""
+For example, {1,2,3} intially we have an emtpy set as result [ [ ] ]
+Considering 1, if not use it, still [ ], if use 1, add it to [ ], so we have [1] now
+Combine them, now we have [ [ ], [1] ] as all possible subset
+
+Next considering 2, if not use it, we still have [ [ ], [1] ], if use 2, just add 2 to each previous subset, we have [2], [1,2]
+Combine them, now we have [ [ ], [1], [2], [1,2] ]
+
+Next considering 3, if not use it, we still have [ [ ], [1], [2], [1,2] ], if use 3, just add 3 to each previous subset, we have [ [3], [1,3], [2,3], [1,2,3] ]
+Combine them, now we have [ [ ], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3] ]
+
+"""
+
+# Iteratively
+def subsets(self, nums):
+    res = [[]]
+    for num in sorted(nums):
+        res += [item+[num] for item in res]
+    return res
+    
