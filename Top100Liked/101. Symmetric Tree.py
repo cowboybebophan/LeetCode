0750a6f563f5ba205e471016ca2e9d3edb3791f9
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-06-10 16:23:51 -0400

    Create 101. Symmetric Tree.py

---
+++ Top100Liked/101. Symmetric Tree.py
@@ -0,0 +1,22 @@
+"""
+https://leetcode.com/problems/symmetric-tree/discuss/33068/6line-AC-python
+
+"""
+
+# Iteratively
+class Solution:
+    def isSymmetric(self, root):
+        queue = [root]
+        while queue:
+            values = [i.val if i else None for i in queue]
+            if values != values[::-1]: return False
+            queue = [child for i in queue if i for child in (i.left, i.right)]
+        return True
+      
+# Recursively
+    def isSymmetric(self, root):
+        def isSym(L,R):
+            if L and R and L.val == R.val: 
+                return isSym(L.left, R.right) and isSym(L.right, R.left)
+            return L == R
+        return not root or isSym(root.left, root.right)
