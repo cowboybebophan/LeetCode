fe4c6d3f04f9195eb1e56093f4c65960d6b81497
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-06-21 16:17:27 -0400

    Create 136. Single Number.py

---
+++ Top100Liked/136. Single Number.py
@@ -0,0 +1,13 @@
+"""
+^ : 按位异或运算符：当两对应的二进位相异时，结果为1
+60 ^ 13 = 0011 1100 ^ 0000 1101 = 0011 0001 = 49
+又因为a ^ b = b ^ a, 所以按位异或运算符可以交换位置: 4 ^ 1 ^ 2 ^ 1 ^ 2 = ( 4 ) ^ (1 ^ 1) ^ (2 ^ 2) = 4 ^ 0 ^ 0 = 4
+
+"""
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
