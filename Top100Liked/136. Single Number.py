08aa1cdb4a7b7893dc2bd5a40afce88329ddc37d
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-06-21 16:24:12 -0400

    Update 136. Single Number.py

--- Top100Liked/136. Single Number.py
+++ Top100Liked/136. Single Number.py
@@ -1,7 +1,13 @@
 """
+caikehe大神的答案： https://leetcode.com/problems/single-number/discuss/43000/Python-different-solutions.
+
+Using bitwise XOR:
+
 ^ : 按位异或运算符：当两对应的二进位相异时，结果为1
 60 ^ 13 = 0011 1100 ^ 0000 1101 = 0011 0001 = 49
-又因为a ^ b = b ^ a, 所以按位异或运算符可以交换位置: 4 ^ 1 ^ 2 ^ 1 ^ 2 = ( 4 ) ^ (1 ^ 1) ^ (2 ^ 2) = 4 ^ 0 ^ 0 = 4
+
+又因为a ^ b = b ^ a, (XOR is commutative)
+所以按位异或运算符可以交换位置: 4 ^ 1 ^ 2 ^ 1 ^ 2 = ( 4 ) ^ (1 ^ 1) ^ (2 ^ 2) = 4 ^ 0 ^ 0 = 4
 
 """
 
