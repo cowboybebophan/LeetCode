e6b39245763919122bc7c1a67ceec6c7321b1654
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-06-26 15:33:44 -0400

    Update 053. Maximum Subarray.py

--- Top100Liked/053. Maximum Subarray.py
+++ Top100Liked/053. Maximum Subarray.py
@@ -1,5 +1,7 @@
 """
-This solution comes from Kadane's algorithm:
+Solution 1 uses dynamic programming. Same idea as problem 152. Maximum Product subarray.
+
+Solution 2 comes from Kadane's algorithm:
 https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
 
 Note: this function will perform in-place modification.
@@ -12,6 +14,25 @@ For exmaple: [-2,1,-3,4,-1,2,1,-5,4] --> [-2,1,-2,4,3,5,6,1,5]
 
 """
 
+# Solution 1
+
+class Solution:
+    def maxSubArray(self, nums):
+        if not nums:
+            return None
+        # maxSum stores the max sum we have found so far
+        # imax stores the max sum of subarray that ends with 
+        # the current number nums[i]
+        
+        maxSum = imax  = nums[0]
+        
+        for i in range(1, len(nums)):
+            imax = max (nums[i], imax + nums[i] )
+            maxSum = max(maxSum, imax)
+        return maxSum
+
+# Solution 2
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         for i in range(1,len(nums)):
@@ -19,9 +40,11 @@ class Solution:
                 nums[i] += nums[i-1]
         return max(nums)
 
+
 """
 If we want to find out the starting and ending index of the maximum subarray as well, 
 we could write the code like this: 
+
 """
 
 class Solution:
