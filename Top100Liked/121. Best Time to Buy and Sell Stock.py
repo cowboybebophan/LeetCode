dc1483af3779731f4be6292f6d3177fa6c8b6510
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-06-27 12:49:12 -0400

    Update 121. Best Time to Buy and Sell Stock.py

--- Top100Liked/121. Best Time to Buy and Sell Stock.py
+++ Top100Liked/121. Best Time to Buy and Sell Stock.py
@@ -1,4 +1,5 @@
 """
+Solution 1:
 Idea comes from: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/
 
 The logic to solve this problem is same as "max subarray problem" using Kadane's Algorithm.
@@ -10,8 +11,12 @@ and find a contiguous subarray giving maximum profit. If the difference falls be
 ma_cur:    [0,0,4,2,5,3] (reset the total difference to 0 if it falls below 0)
 ma_so_far: [0,0,4,4,5,5]
 
+Solution 2:
+We calculate the min_price before prices[i], then the max profit if we sell the stock on day i is (prices[i] - min_price).
+
 """
 
+# Solution 1
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_cur, max_so_far = 0, 0 
@@ -19,3 +24,14 @@ class Solution:
             max_cur = max(0, max_cur + prices[i]- prices[i-1])
             max_so_far = max(max_so_far, max_cur)
         return max_so_far
+
+# Solution 2
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = float('inf')
+        for i in range(len(prices)):
+            min_price = min(min_price, prices[i])
+            profit = prices[i] - min_price
+            max_profit = max(max_profit, profit)
+        return max(0, max_profit)
