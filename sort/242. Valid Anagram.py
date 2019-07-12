4a7aa0954a8ad0b00033adf8aa7944708d187d14
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-07-12 12:29:46 -0400

    Create 242. Valid Anagram.py

---
+++ sort/242. Valid Anagram.py
@@ -0,0 +1,19 @@
+# sorting
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+# dictionary
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dic = {}
+        for item in s:
+            dic[item] = dic.get(item, 0) + 1
+        for item in t:
+            if item not in dic:
+                return False
+            dic[item] -= 1
+        for k in dic:
+            if dic[k] != 0:
+                return False
+        return True
