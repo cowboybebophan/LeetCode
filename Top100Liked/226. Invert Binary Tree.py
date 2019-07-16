ffc42ebdc3bd0f81bda16e5a4074f11c8cb8cc3f
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-07-16 14:07:26 -0400

    Create 226. Invert Binary Tree.py

---
+++ Top100Liked/226. Invert Binary Tree.py
@@ -0,0 +1,20 @@
+# Iteratively
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack += [node.left, node.right]
+        return root
+        
+# Recursively
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = root.right, root.left
+            self.invertTree(root.left)
+            self.invertTree(root.right)
+        return root
+    
