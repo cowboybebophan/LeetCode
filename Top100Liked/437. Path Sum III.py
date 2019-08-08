829fca5b79037acda330bdfd4c9d23e2e4dba21e
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-08 01:03:41 -0400

    Create 437. Path Sum III.py

---
+++ Top100Liked/437. Path Sum III.py
@@ -0,0 +1,34 @@
+# Brute Force
+
+class Solution:
+    def pathSum(self, root, sum):
+        if root:
+            return self.findPath(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+        return 0
+        
+    def findPath(self, node, target):
+        if node:
+            return int(node.val == target) + self.findPath(node.left, target - node.val) + self.findPath(node.right, target - node.val)
+        return 0
+
+# Two Sum
+
+class Solution(object):
+    def pathSum(self, root, target):
+        self.result = 0
+        cache = {0:1}
+        
+        self.dfs(root, target, 0, cache)
+        return self.result
+    
+    def dfs(self, root, target, currPathSum, cache):
+        if root is None:
+            return
+        currPathSum += root.val
+        oldPathSum = currPathSum - target
+        self.result += cache.get(oldPathSum, 0)
+        cache[currPathSum] = cache.get(currPathSum, 0) + 1
+      
+        self.dfs(root.left, target, currPathSum, cache)
+        self.dfs(root.right, target, currPathSum, cache)
+        cache[currPathSum] -= 1
