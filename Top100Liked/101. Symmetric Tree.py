6a37a6b161bad61318dde1d6f8754a6bb160a4fc
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-12 14:47:01 -0400

    Update 101. Symmetric Tree.py

--- Top100Liked/101. Symmetric Tree.py
+++ Top100Liked/101. Symmetric Tree.py
@@ -6,11 +6,12 @@ https://leetcode.com/problems/symmetric-tree/discuss/33068/6line-AC-python
 # Iteratively
 class Solution:
     def isSymmetric(self, root):
-        queue = [root]
-        while queue:
-            values = [i.val if i else None for i in queue]
-            if values != values[::-1]: return False
-            queue = [child for i in queue if i for child in (i.left, i.right)]
+        level = [root]
+        while level:
+            values = [node.val if node else None for node in level]
+            if values != values[::-1]: 
+                return False
+            level = [child for node in level if node for child in (node.left, node.right)]
         return True
       
 # Recursively
@@ -18,5 +19,17 @@ class Solution:
         def isSym(L,R):
             if L and R and L.val == R.val: 
                 return isSym(L.left, R.right) and isSym(L.right, R.left)
-            return L == R
+            return L == R   # means L == None == R
+        return not root or isSym(root.left, root.right)
+    
+    or
+  
+    def isSymmetric(self, root):
+        def isSym(L,R):
+            if L and R and L.val == R.val: 
+                return isSym(L.left, R.right) and isSym(L.right, R.left)
+            elif L == R == None:
+                return True
+            else:
+                return False
         return not root or isSym(root.left, root.right)
