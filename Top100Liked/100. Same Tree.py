1965e8545a904925bc174140f1ca163578703265
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-15 10:55:44 -0400

    Create 100. Same Tree.py

---
+++ Top100Liked/100. Same Tree.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)      
+        else:
+            return p == q  # when one of p and q is None or p and q are both None
