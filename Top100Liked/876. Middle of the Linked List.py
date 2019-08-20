c860424703a574c4e55b70947aacbc301494bede
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-20 18:19:10 -0400

    Create 876. Middle of the Linked List.py

---
+++ Top100Liked/876. Middle of the Linked List.py
@@ -0,0 +1,10 @@
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+    
+        slow = fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        return slow
