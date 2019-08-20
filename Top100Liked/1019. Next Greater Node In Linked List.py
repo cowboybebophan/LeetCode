2b666a6dd09138fc90ec5b7e53130d7e8f22408c
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-20 18:19:44 -0400

    Create 1019. Next Greater Node In Linked List.py

---
+++ Top100Liked/1019. Next Greater Node In Linked List.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextLargerNodes(self, head):
+        
+        res, stack = [], []
+        
+        while head:
+            while stack and stack[-1][1] < head.val:
+                res[stack.pop()[0]] = head.val
+            stack.append([len(res), head.val])
+            res.append(0)
+            head = head.next
+        return res
