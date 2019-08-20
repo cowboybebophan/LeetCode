09122517e8ddef4f3eca956a74eaf0c76e9de54f
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-20 18:42:59 -0400

    Update 1019. Next Greater Node In Linked List.py

--- Top100Liked/1019. Next Greater Node In Linked List.py
+++ Top100Liked/1019. Next Greater Node In Linked List.py
@@ -1,8 +1,8 @@
 class Solution:
     def nextLargerNodes(self, head):
         
-        res, stack = [], []
-        
+        res, stack = [], [] # Be carefule here, do Not initialize like this: res = stack = []
+                            # because res and stack points to the same reference and they will change together as [] changes.
         while head:
             while stack and stack[-1][1] < head.val:
                 res[stack.pop()[0]] = head.val
