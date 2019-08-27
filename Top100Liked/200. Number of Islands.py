9fbae6b44ba9b685349c7b01069e6b748d2913d0
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-26 20:19:03 -0400

    Update 200. Number of Islands.py

--- Top100Liked/200. Number of Islands.py
+++ Top100Liked/200. Number of Islands.py
@@ -11,7 +11,7 @@ When self.dfs(i,j) returns, we know that we have explored the island that grid[i
 Therefore, count += 1 and we go to the next element.
 
 """
-
+# DFS
 class Solution(object):
     def numIslands(self, grid):
         if not grid:
@@ -32,3 +32,27 @@ class Solution(object):
         self.dfs(grid, i-1, j)
         self.dfs(grid, i, j+1)
         self.dfs(grid, i, j-1)
+
+# BFS
+class Solution:
+    def numIslands(self, grid):
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.bfs(grid, i, j)
+                    count += 1
+        return count
+    
+    def bfs(self, grid, i, j):
+        q = collections.deque([])
+        q.append((i,j))
+        grid[i][j] = '0'
+        while q:
+            x, y = q.popleft()
+            dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            for d in dirs:
+                nx, ny = x + d[0], y + d[1]
+                if 0 <= nx <len(grid) and 0<= ny <len(grid[0]) and grid[nx][ny] == '1':
+                    q.append((nx,ny))
+                    grid[nx][ny] = '0'
