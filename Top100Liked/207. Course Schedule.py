8775eff0a7118fe3c98915464b7f2798e82239ac
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-28 00:06:30 -0400

    Update 207. Course Schedule.py

--- Top100Liked/207. Course Schedule.py
+++ Top100Liked/207. Course Schedule.py
@@ -34,3 +34,34 @@ class Solution:
                 if degree[j] == 0:
                     zeroDegree.append(j)
         return len(zeroDegree) == n
+
+# DFS
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        visited = [0 for _ in range(numCourses)]
+        
+        for x, y in prerequisites:
+            graph[x].append(y)
+        
+        for i in range(numCourses):
+            if not self.dfs(graph, visited, i):
+                return False
+        return True
+    
+    
+    def dfs(self, graph, visited, i):
+        if visited[i] == -1:
+            return False
+        if visited[i] == 1:
+            return True
+        visited[i] = -1
+        
+        for j in graph[i]:
+            if not self.dfs(graph, visited,j):
+                return False
+        
+        visited[i] = 1
+        return True
+        
