73616892f34eb0e1420d09fbdae04e4ff295a142
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-31 16:05:00 -0400

    Update 347. Top K Frequent Elements.py

--- Top100Liked/347. Top K Frequent Elements.py
+++ Top100Liked/347. Top K Frequent Elements.py
@@ -1,14 +1,10 @@
 # Dictionary + quickSelect
 
+from collections import Counter
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if not nums:
-            return []
-        
-        count = {}
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
-        count = list(count.items())
+        count = list(Counter(nums).items())
         
         self.quickSelect(count, 0, len(count) - 1, k)
         
