9971a2db23a0c0b6de546fcf5e140a49d080f701
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-08-31 14:31:36 -0400

    Update 215. Kth Largest Element in an Array.py

--- Top100Liked/215. Kth Largest Element in an Array.py
+++ Top100Liked/215. Kth Largest Element in an Array.py
@@ -26,6 +26,35 @@ class Solution(object):
             res = heapq.heappop(nums)
         return -res
         
- # Method 4
- # Using quickSelect/quickSort
+# Method 4
+# Using quickSelect/quickSort
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        self.quickSort(nums, 0, len(nums) - 1, k)
+        return nums[k-1]
+    
+    
+    def partition(self, nums, low, high):
+        pivot = nums[high]
+        i = low - 1
+        
+        for j in range(low, high):
+            if nums[j] > pivot:
+                i += 1
+                nums[j], nums[i] = nums[i], nums[j]
+        
+        nums[i+1], nums[high] = nums[high], nums[i+1]
+        return i + 1
+    
+    def quickSort(self, nums, low, high, k):
+        if low < high:
+            p = self.partition(nums, low, high)
+            if p == k - 1:
+                return
+            elif p > k - 1:
+                self.quickSort(nums, low, p - 1, k)
+            else:
+                self.quickSort(nums, p + 1, high, k)
+
  
