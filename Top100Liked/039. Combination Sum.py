57c9760a6e6e0a44171dd6ad5d33c62f3534f209
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-03 14:40:13 -0400

    Update 039. Combination Sum.py

--- Top100Liked/039. Combination Sum.py
+++ Top100Liked/039. Combination Sum.py
@@ -29,3 +29,20 @@ class Solution:
         return res  
         
         # Use candidates[i:] to avoid duplicate combinations like [2,2,3] and [2,3,2]
+
+        
+# Solution 2
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        for i, num in enumerate(candidates):
+            if num <= target:
+                self.helper(candidates[i:], target - num, res, [num])
+        return res
+    
+    def helper(self, candidates, target, res, path):
+        if target == 0:
+            res.append(path)
+        for i, num in enumerate(candidates):
+                if num <= target:
+                    self.helper(candidates[i:], target - num, res, path + [num])
