a7c6d71977186fc5b7927c26ff1c365ce8e78f63
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-03 16:33:37 -0400

    Update 078. Subsets.py

--- Top100Liked/078. Subsets.py
+++ Top100Liked/078. Subsets.py
@@ -1,4 +1,29 @@
 """
+https://leetcode.com/problems/subsets/discuss/27301/Python-easy-to-understand-solutions-(DFS-recursively-Bit-Manipulation-Iteratively).
+"""
+
+# DFS + Recursion
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(nums, 0, res, [])
+        return res
+        
+    def dfs(self, nums, index, res, path):
+        res.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, i + 1, res, path + [nums[i]])
+
+# Iteratively
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for num in nums:
+            res += [item+[num] for item in res]
+        return res
+    
+"""
 For example, {1,2,3} intially we have an emtpy set as result [ [ ] ]
 Considering 1, if not use it, still [ ], if use 1, add it to [ ], so we have [1] now
 Combine them, now we have [ [ ], [1] ] as all possible subset
@@ -10,12 +35,3 @@ Next considering 3, if not use it, we still have [ [ ], [1], [2], [1,2] ], if us
 Combine them, now we have [ [ ], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3] ]
 
 """
-
-# Iteratively
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for num in nums:
-            res += [item+[num] for item in res]
-        return res
-    
