81434271c71f621e0b8f1329f491955a0a405327
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-03 16:53:40 -0400

    Update 090. Subsets II.py

--- Top100Liked/090. Subsets II.py
+++ Top100Liked/090. Subsets II.py
@@ -1,4 +1,24 @@
 """
+https://leetcode.com/problems/subsets-ii/discuss/30305/Simple-python-solution-(DFS).
+"""
+# DFS + Recursion
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        self.dfs(nums, 0, res, [])
+        return res
+    
+    def dfs(self, nums, index, res, path):
+        res.append(path)
+        
+        for i in range(index, len(nums)):
+            if i > index and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums, i + 1, res, path + [nums[i]])
+         
+"""
 The difference between Subsets II and Subsets is that nums might contain duplicates.
 
 We sort the list first making sure that duplicates are adjacent. 
