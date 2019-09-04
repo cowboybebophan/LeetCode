231247431d604d01737018f9be242de64fdc0616
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-03 21:02:12 -0400

    Create 047. Permutations II.py

---
+++ Top100Liked/047. Permutations II.py
@@ -0,0 +1,19 @@
+"""
+https://leetcode.com/problems/permutations-ii/discuss/18649/Python-easy-to-understand-backtracking-solution.
+"""
+# DFS + Recursion / Backtracking
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        self.dfs(nums, res, [])
+        return res
+    
+    def dfs(self, nums, res, path):
+        if not nums:
+            res.append(path)
+            return
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums[:i] + nums[i+1:], res, path + [nums[i]])
