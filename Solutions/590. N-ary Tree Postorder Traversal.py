60f449ddba2772fc97e53ea1e60d7feb1633a766
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-09 12:22:08 -0400

    Update 590. N-ary Tree Postorder Traversal.py

--- Solutions/590. N-ary Tree Postorder Traversal.py
+++ Solutions/590. N-ary Tree Postorder Traversal.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
         res = []
@@ -9,3 +10,14 @@ class Solution:
                 res.append(node.val)
                 stack.extend(node.children)
         return res[::-1]
+    
+# Solution 2
+class Solution(object):
+    def postorder(self, root):
+        stack, res = [root], []
+        
+        while stack and root:
+            n = stack.pop()
+            res.insert(0, n.val)
+            stack += n.children
+        return res
