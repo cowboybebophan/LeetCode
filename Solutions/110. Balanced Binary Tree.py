f5b7c0e4854671b17385414651b288d9acfeeae0
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-09 14:09:22 -0400

    Update 110. Balanced Binary Tree.py

--- Solutions/110. Balanced Binary Tree.py
+++ Solutions/110. Balanced Binary Tree.py
@@ -2,7 +2,7 @@ class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
         if not root:
             return True
-        return abs(self.maxDepth(root.left) - self.maxDepth(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
+        return abs(self.maxDepth(root.left) - self.maxDepth(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
             
     
     def maxDepth(self, node):
