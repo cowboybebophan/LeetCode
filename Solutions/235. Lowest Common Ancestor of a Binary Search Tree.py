26cf49a8dca4ab2d206d13de63cc8f509905442e
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-11 19:58:56 -0400

    Update 235. Lowest Common Ancestor of a Binary Search Tree.py

--- Solutions/235. Lowest Common Ancestor of a Binary Search Tree.py
+++ Solutions/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -1,8 +1,11 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # If both p and q are greater than parent
         if root.val < p.val and root.val < q.val:
             return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are lesser than parent
         elif root.val > p.val and root.val > q.val:
             return self.lowestCommonAncestor(root.left, p, q)
-        else: # We find a split point here
+        # We have found the split point, i.e. the LCA node.
+        else: 
             return root
