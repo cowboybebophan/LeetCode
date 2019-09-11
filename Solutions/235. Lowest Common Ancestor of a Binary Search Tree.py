49f6973c987cb9ced3df8600fd125bfd16347b31
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-11 19:58:05 -0400

    Create 235. Lowest Common Ancestor of a Binary Search Tree.py

---
+++ Solutions/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else: # We find a split point here
+            return root
