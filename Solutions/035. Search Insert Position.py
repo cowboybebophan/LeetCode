097d854bf7a5d50ccb977f9aef22a02441cc4151
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-13 17:17:26 -0400

    Update 035. Search Insert Position.py

--- Solutions/035. Search Insert Position.py
+++ Solutions/035. Search Insert Position.py
@@ -1,12 +1,12 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l , r = 0, len(nums)-1
-        while l <= r:
-            mid=(l+r)//2
-            if nums[mid]== target:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
                 return mid
-            if nums[mid] < target:
-                l = mid+1
+            elif nums[mid] > target:
+                high = mid - 1
             else:
-                r = mid-1
-        return l
+                low = mid + 1
+        return low
