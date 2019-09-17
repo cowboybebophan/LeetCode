e207a507faa2d2864dd72d81a556cc3ed0aeb19d
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-17 12:08:40 -0400

    Update 011. Container With Most Water.py

--- Solutions/011. Container With Most Water.py
+++ Solutions/011. Container With Most Water.py
@@ -12,3 +12,17 @@ class Solution:
  If height[L] < height[R], move L, else move R. Say height[0] < height[5],
  area of (0, 4), (0, 3), (0, 2), (0, 1) will be smaller than (0, 5), so no need to try them.
  
+# Or
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res, L, R, width = 0, 0, len(height) - 1, len(height) - 1
+        while L < R:
+            if height[L] < height[R]:
+                res = max(res, width * height[L])
+                L += 1
+            else:
+                res = max(res, width * height[R])
+                R -= 1
+            width -= 1
+        return res
