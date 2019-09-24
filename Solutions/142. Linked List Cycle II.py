847521119376419b0d763bce704ac64007b3e6bd
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-09-23 20:31:34 -0400

    Update 142. Linked List Cycle II.py

--- Solutions/142. Linked List Cycle II.py
+++ Solutions/142. Linked List Cycle II.py
@@ -1,3 +1,17 @@
+"""
+To understand this solution, you just need to ask yourself these question.
+Assume the distance from head to the start of the loop is x
+the distance from the start of the loop to the point fast and slow meet is y
+the distance from the point fast and slow meet to the start of the loop is z
+What is the distance fast moved? What is the distance slow moved? And their relationship?
+
+1. x + y + z + y
+2. x + y
+3. x + y + z + y = 2 (x + y)
+
+Thus x = z
+
+"""
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
         slow = fast = head
