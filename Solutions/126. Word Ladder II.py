4adaac20dbb8ab9c5fa6803a2f71699d91f3fa95
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-10-17 00:55:53 -0400

    Update 126. Word Ladder II.py

--- Solutions/126. Word Ladder II.py
+++ Solutions/126. Word Ladder II.py
@@ -13,12 +13,12 @@ class Solution:
             for word in layer:
                 if word == endWord:
                     res.extend(l for l in layer[word])
-                else:    
-                    for i in range(len(word)):
-                        for ch in alpha:
-                            newWord = word[:i] + ch + word[i+1:]
-                            if newWord in wordList:
-                                newLayer[newWord] += [path + [newWord] for path in layer[word]]
+                    return res 
+                for i in range(len(word)):
+                    for ch in alpha:
+                        newWord = word[:i] + ch + word[i+1:]
+                        if newWord in wordList:
+                            newLayer[newWord] += [path + [newWord] for path in layer[word]]
             wordList -= set(newLayer.keys())
             layer = newLayer
         return res
