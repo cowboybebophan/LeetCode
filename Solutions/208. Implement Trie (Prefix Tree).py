a40df05a3c422b71d4231ba3b763e4fd3d8446fb
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-10-24 17:06:50 -0400

    Update 208. Implement Trie (Prefix Tree).py

--- Solutions/208. Implement Trie (Prefix Tree).py
+++ Solutions/208. Implement Trie (Prefix Tree).py
@@ -2,42 +2,36 @@
 Credit goes to: https://leetcode.com/problems/implement-trie-prefix-tree/discuss/58989/My-python-solution
 
 """
-
 class TrieNode:
     def __init__(self):
-        self.isWord = False
+        self.word = False
         self.children = {}
-
+        
 class Trie:
-
     def __init__(self):
         self.root = TrieNode()
         
-
     def insert(self, word: str) -> None:
         node = self.root
-        for w in word:
-            if w not in node.children:
-                node.children[w] = TrieNode()
-            node = node.children[w]
-        node.isWord = True
-            
-
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = True
+        
     def search(self, word: str) -> bool:
         node = self.root
-        for w in word:
-            if w not in node.children:
+        for char in word:
+            if char not in node.children:
                 return False
-            node = node.children[w]
-        return node.isWord
-        
-        
+            node = node.children[char]
+        return node.word     
 
     def startsWith(self, prefix: str) -> bool:
         node = self.root
-        for w in prefix:
-            if w not in node.children:
+        for char in prefix:
+            if char not in node.children:
                 return False
-            node = node.children[w]
-        return True
+            node = node.children[char]
+        return True 
         
