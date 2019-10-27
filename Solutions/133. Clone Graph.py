87d360a524536046c2981e9c18b83fe814635f37
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-10-26 21:57:55 -0400

    Update 133. Clone Graph.py

--- Solutions/133. Clone Graph.py
+++ Solutions/133. Clone Graph.py
@@ -1,22 +1,23 @@
 # Same idea as problem 138.Copy List with Random Pointer
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
+    def cloneGraph(self, node):
         self.visited = {}
         return self.deepCopy(node)
-
-    def deepCopy(self, node):
+    
+    def deepCopy(self, node): # input is the node, returns the copy of the node
         if not node:
             return None
+        
         if node in self.visited:
             return self.visited[node]
         
         copy = Node(node.val, [])
         self.visited[node] = copy
         
-        for i in node.neighbors:
-            if i in self.visited:
-                copy.neighbors.append(self.visited[i])
+        for n in node.neighbors:
+            if n in self.visited:
+                copy.neighbors.append(self.visited[n])
             else:
-                copy.neighbors.append(self.deepCopy(i))
-        
+                copy.neighbors.append(self.deepCopy(n))
+        self.visited[node] = copy
         return copy
