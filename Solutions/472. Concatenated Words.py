d500063e3b31dca001fe152d4fc4b836db29bf2a
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-10-29 17:07:36 -0400

    Create 472. Concatenated Words.py

---
+++ Solutions/472. Concatenated Words.py
@@ -0,0 +1,25 @@
+# DFS
+# https://leetcode.com/problems/concatenated-words/discuss/159348/Python-DFS-readable-solution
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        new_words = set(words)
+        
+        def dfs(word):
+            for i in range(1, len(word)):
+                prefix = word[:i]
+                suffix = word[i:]
+                
+                if prefix in new_words and suffix in new_words:
+                    return True
+                if prefix in new_words and dfs(suffix):
+                    return True
+            
+            return False
+        
+        res = []
+        for word in new_words:
+            if dfs(word):
+                res.append(word)
+        return res
+        
+# Trie
