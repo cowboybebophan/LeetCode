5ec64a8488516df122d97bd8fd1a60ff5c3e7e51
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-11-05 13:55:44 -0500

    Update 257. Binary Tree Paths.py

--- Solutions/257. Binary Tree Paths.py
+++ Solutions/257. Binary Tree Paths.py
@@ -1,5 +1,4 @@
-# Recursively
-
+# DFS Recursively
 class Solution:
     def binaryTreePaths(self, root: TreeNode) -> List[str]:
         if not root:
@@ -16,3 +15,38 @@ class Solution:
         if node.right:
             self.findPath(node.right, res, path + "->" + str(node.right.val))
             
+# DFS + stack
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        if not root:
+            return []
+        res, stack = [], [(root, "")]
+        
+        while stack:
+            node, path = stack.pop()
+            if not (node.left or node.right):
+                res.append(path + str(node.val))
+            if node.left:
+                stack.append((node.left, path + str(node.val) + "->"))
+            if node.right:
+                stack.append((node.right, path + str(node.val) + "->"))
+        
+        return res
+    
+# BFS + queue
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        if not root:
+            return []
+        res, queue = [], collections.deque([(root, "")])
+        
+        while queue:
+            node, path = queue.popleft()
+            if not (node.left or node.right): # current node is leaf
+                res.append(path + str(node.val))
+            if node.left:
+                queue.append((node.left, path + str(node.val) + "->"))
+            if node.right:
+                queue.append((node.right, path + str(node.val) + "->"))
+        
+        return res
