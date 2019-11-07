175883195b497b10c87cafbc8f15d65c74e2dc38
cowboybebophan <33766514+cowboybebophan@users.noreply.github.com>
2019-11-06 21:01:07 -0500

    Update 472. Concatenated Words.py

--- Solutions/472. Concatenated Words.py
+++ Solutions/472. Concatenated Words.py
@@ -2,14 +2,14 @@
 # https://leetcode.com/problems/concatenated-words/discuss/159348/Python-DFS-readable-solution
 class Solution(object):
     def findAllConcatenatedWordsInADict(self, words):
-        new_words = set(words)
+        new_words = set(words)  # O(N)
         
         def dfs(word):
             for i in range(1, len(word)):
                 prefix = word[:i]
                 suffix = word[i:]
                 
-                if prefix in new_words and suffix in new_words:
+                if prefix in new_words and suffix in new_words:  # set lookups are O(1)
                     return True
                 if prefix in new_words and dfs(suffix):
                     return True
@@ -18,7 +18,7 @@ class Solution(object):
         
         res = []
         for word in new_words:
-            if dfs(word):
+            if dfs(word): 
                 res.append(word)
         return res
         
